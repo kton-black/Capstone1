7f7f4e36236cac9b83dca4517e07c004524adf53
Keaton Black <ktbcxc@umsystem.edu>
2023-03-08 11:56:22 -0600

    Displays Board

--- Scrabble/scrabble/Scrabble.py
+++ Scrabble/scrabble/Scrabble.py
@@ -1,139 +1,157 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import random
+import pygame
+from scrabble.board import Scrabble_Board
 
-#List of all letters in scrabble
-player_letters = ['E','E','E','E','E','E','E','E','E','E','E','E','A','A','A','A','A','A','A','A','A','I','I','I','I','I','I','I','I','I','O','O','O','O','O','O','O','O','N','N','N','N','N','N','R','R','R','R','R','R','T','T','T','T','T','T','L','L','L','L','S','S','S','S','U','U','U','U','D','D','D','D','G','G','G','B','B','C','C','M','M','P','P','F','F','H','H','V','V','W','W','Y','Y','K','J','X','Q','Z','_','_']
-letters_in_hand = []
-#letters_in_hand[7]
+def main():
+    #List of all letters in scrabble
+    player_letters = ['E','E','E','E','E','E','E','E','E','E','E','E','A','A','A','A','A','A','A','A','A','I','I','I','I','I','I','I','I','I','O','O','O','O','O','O','O','O','N','N','N','N','N','N','R','R','R','R','R','R','T','T','T','T','T','T','L','L','L','L','S','S','S','S','U','U','U','U','D','D','D','D','G','G','G','B','B','C','C','M','M','P','P','F','F','H','H','V','V','W','W','Y','Y','K','J','X','Q','Z','_','_']
+    letters_in_hand = []
+    #letters_in_hand[7]
 
-#Variables
-lst = []
-score = 0
-total = 0
-i = 0
-count = 0
+    #Variables
+    lst = []
+    score = 0
+    total = 0
+    i = 0
+    count = 0
 
-#grabs username
-username: str = input("Please enter a username: ")
-print("Is-", username, "-playing? If yes, type Yes, if no, type No: ")
-player = input()
-print("\n\n\n")
+    screen = pygame.display.set_mode((1000, 600))
+    background = pygame.Surface((1000, 1000))
+    background.fill((255, 0, 255))
+    screen.blit(background, (0, 0))
+    board = Scrabble_Board(screen)
+    board.create_board()
+    # board.display(screen)
+    pygame.display.flip()
 
-#Keep total for user playing
-if player == "Yes":
+    # screen.blit(pygame.transform.scale(board.window, (1000, 1000)), (0, 0))
 
-    #Keeping track of user's points
-    while player == "Yes":
+    #grabs username
+    # username: str = input("Please enter a username: ")
+    # print("Is-", username, "-playing? If yes, type Yes, if no, type No: ")
+    # player = input()
+    # print("\n\n\n")
 
-        #Grabbing letters from the letter pool
-        for randomletter in player_letters:
-            while count <= 7:
-                if count == 7:
-                    break
-                else: 
-                    randomletter = random.choices(player_letters)
-                    letters_in_hand.append(randomletter)
-                    count = count + 1
-        
-        print("Here are the letters you currently have: ", letters_in_hand) 
-        
-        #Resetting Score
-        score = 0
+    board.start()
 
-        #Getting the input for the word the user would like to use
-        WordInput: str = input("Please Enter a valid word With the letters you received (If you have a blank tile, use an underscore in the loaction of where you'd like to use it): \n")
-
-        #Making letters uppercase
-        Word = WordInput.upper()
-
-        #Making sure the word is within the space allowed
-        if len(Word) > 15:
-            print("Invalid word, must be smaller than 15 letters")
-        else:
-            print("\n\nYou typed the word: ", Word)
-
-            #For an i in the word
-            for letter in Word:
-                #Split the up the word
-                lst.append(letter)
-                temp = 0
-
-                #Finding the letter and change it to its numerical value
-                #Blank tile is worth 0 points
-                if lst[i] == '_':
-                    score = score + 0
-                if lst[i] == 'A':
-                    score = score + 1
-                if lst[i] == 'E':
-                    score = score + 1
-                if lst[i] == 'I':
-                    score = score + 1
-                if lst[i] == 'L':
-                    score = score + 1
-                if lst[i] == 'N':
-                    score = score + 1
-                if lst[i] == 'O':
-                    score = score + 1
-                if lst[i] == 'R':
-                    score = score + 1
-                if lst[i] == 'S':
-                    score = score + 1
-                if lst[i] == 'T':
-                    score = score + 1
-                if lst[i] == 'U':
-                    score = score + 1
-                if lst[i] == 'D':
-                    score = score + 2
-                if lst[i] == 'G':
-                    score = score + 2
-                if lst[i] == 'B':
-                    score = score + 3
-                if lst[i] == 'C':
-                    score = score + 3
-                if lst[i] == 'M':
-                    score = score + 3
-                if lst[i] == 'P':
-                    score = score + 3
-                if lst[i] == 'F':
-                    score = score + 4
-                if lst[i] == 'H':
-                    score = score + 4
-                if lst[i] == 'V':
-                    score = score + 4
-                if lst[i] == 'W':
-                    score = score + 4
-                if lst[i] == 'Y':
-                    score = score + 4
-                if lst[i] == 'K':
-                    score = score + 5
-                if lst[i] == 'J':
-                    score = score + 8
-                if lst[i] == 'X':
-                    score = score + 8
-                if lst[i] == 'Q':
-                    score = score + 10
-                if lst[i] == 'Z':
-                    score = score + 10
-
-                #Increments through the list
-                i += 1
-                temp = score
-        
-        print("The score of your word is: ", score) 
-
-        #Gathering the total after every 'round'
-        total = total + temp 
-        print(username, "total score is: ", total, "\n\n")
-
-        print("Is", username, "Still playing? If yes, type Yes. If not, type No:")
-        player = input()
-        print("\n\n")
-
-elif player == "No":
-    print("Don't cheat!")
-
-else:
-    print("Invalid Input")
+    #Keep total for user playing
+    # if player == "Yes":
+    #
+    #     #Keeping track of user's points
+    #     while player == "Yes":
+    #
+    #         #Grabbing letters from the letter pool
+    #         for randomletter in player_letters:
+    #             while count <= 7:
+    #                 if count == 7:
+    #                     break
+    #                 else:
+    #                     randomletter = random.choices(player_letters)
+    #                     letters_in_hand.append(randomletter)
+    #                     count = count + 1
+    #
+    #         print("Here are the letters you currently have: ", letters_in_hand)
+    #
+    #         #Resetting Score
+    #         score = 0
+    #
+    #         #Getting the input for the word the user would like to use
+    #         WordInput: str = input("Please Enter a valid word With the letters you received (If you have a blank tile, use an underscore in the loaction of where you'd like to use it): \n")
+    #
+    #         #Making letters uppercase
+    #         Word = WordInput.upper()
+    #
+    #         #Making sure the word is within the space allowed
+    #         if len(Word) > 15:
+    #             print("Invalid word, must be smaller than 15 letters")
+    #         else:
+    #             print("\n\nYou typed the word: ", Word)
+    #
+    #             #For an i in the word
+    #             for letter in Word:
+    #                 #Split the up the word
+    #                 lst.append(letter)
+    #                 temp = 0
+    #
+    #                 #Finding the letter and change it to its numerical value
+    #                 #Blank tile is worth 0 points
+    #                 if lst[i] == '_':
+    #                     score = score + 0
+    #                 elif lst[i] == 'A':
+    #                     score = score + 1
+    #                 elif lst[i] == 'E':
+    #                     score = score + 1
+    #                 elif lst[i] == 'I':
+    #                     score = score + 1
+    #                 elif lst[i] == 'L':
+    #                     score = score + 1
+    #                 elif lst[i] == 'N':
+    #                     score = score + 1
+    #                 elif lst[i] == 'O':
+    #                     score = score + 1
+    #                 elif lst[i] == 'R':
+    #                     score = score + 1
+    #                 elif lst[i] == 'S':
+    #                     score = score + 1
+    #                 elif lst[i] == 'T':
+    #                     score = score + 1
+    #                 elif lst[i] == 'U':
+    #                     score = score + 1
+    #                 elif lst[i] == 'D':
+    #                     score = score + 2
+    #                 elif lst[i] == 'G':
+    #                     score = score + 2
+    #                 elif lst[i] == 'B':
+    #                     score = score + 3
+    #                 elif lst[i] == 'C':
+    #                     score = score + 3
+    #                 elif lst[i] == 'M':
+    #                     score = score + 3
+    #                 elif lst[i] == 'P':
+    #                     score = score + 3
+    #                 elif lst[i] == 'F':
+    #                     score = score + 4
+    #                 elif lst[i] == 'H':
+    #                     score = score + 4
+    #                 elif lst[i] == 'V':
+    #                     score = score + 4
+    #                 elif lst[i] == 'W':
+    #                     score = score + 4
+    #                 elif lst[i] == 'Y':
+    #                     score = score + 4
+    #                 elif lst[i] == 'K':
+    #                     score = score + 5
+    #                 elif lst[i] == 'J':
+    #                     score = score + 8
+    #                 elif lst[i] == 'X':
+    #                     score = score + 8
+    #                 elif lst[i] == 'Q':
+    #                     score = score + 10
+    #                 elif lst[i] == 'Z':
+    #                     score = score + 10
+    #
+    #                 #Increments through the list
+    #                 i += 1
+    #                 temp = score
+    #
+    #         print("The score of your word is: ", score)
+    #
+    #         #Gathering the total after every 'round'
+    #         total = total + temp
+    #         print(username, "total score is: ", total, "\n\n")
+    #
+    #         print("Is", username, "Still playing? If yes, type Yes. If not, type No:")
+    #         player = input()
+    #         print("\n\n")
+    #
+    # elif player == "No":
+    #     print("Don't cheat!")
+    #
+    # else:
+    #     print("Invalid Input")
 
             
+if __name__ == "__main__":
+    main()
--- Scrabble/scrabble/board.py
+++ Scrabble/scrabble/board.py
@@ -1,31 +1,116 @@
 import pygame
-import ConfigParser
+import pathlib
+import configparser
+import sys
 
 class Scrabble_Board(object):
 
-    def __init__(self):
-        pass
-
-    def load_file(self, file="board.ini"):
+    def __init__(self, window, board_width=600, board_height=600, window_width=1000, window_height=600):
+        pygame.init()
         self.board = []
         self.key = {}
-        parser = ConfigParser.ConfigParser()
+        self.width = 0
+        self.length = 0
+
+        self.window = window
+
+        self.load_file()
+
+        # define constants for the board size, window size and grid size
+        self.BOARD_SIZE = (board_width, board_height)
+        self.WINDOW_SIZE = (window_width, window_height)
+        self.GRID_SIZE = 15
+
+        self.flags = pygame.SCALED | pygame.RESIZABLE
+
+
+    def create_board(self):
+        # define colors for the grid and score spaces
+        WHITE = (255, 255, 255)
+        BLACK = (0, 0, 0)
+        RED = (255, 0, 0)
+        LIGHT_RED = (255, 200, 200)
+        BLUE = (0, 0, 255)
+        LIGHT_BLUE = (200, 200, 255)
+        YELLOW = (255, 255, 0)
+
+        # create the window and set its title
+        # self.window = pygame.display.set_mode(self.WINDOW_SIZE, self.flags)
+        pygame.display.set_caption("Scrabble Board")
+
+        self.window.fill((250, 220, 120))
+
+        # draw the grid squares on the window
+        square_size = self.BOARD_SIZE[0] // self.GRID_SIZE
+        for row in range(self.GRID_SIZE):
+            for col in range(self.GRID_SIZE):
+                x = col * square_size
+                y = row * square_size
+                rect = pygame.Rect(x, y, square_size, square_size)
+
+                # fill the square with white
+                pygame.draw.rect(self.window, WHITE, rect)
+
+                # draw light blue squares on double-letter
+                if self.board[row][col] == '@':
+                    pygame.draw.rect(self.window, LIGHT_BLUE, rect)
+
+                # draw light red squares on double-word
+                elif self.board[row][col] == '!':
+                    pygame.draw.rect(self.window, LIGHT_RED, rect)
+
+                # draw blue squares on triple-letter
+                elif self.board[row][col] == '^':
+                    pygame.draw.rect(self.window, BLUE, rect)
+
+                # draw red squares on triple-word
+                elif self.board[row][col] == '$':
+                    pygame.draw.rect(self.window, RED, rect)
+
+                elif self.board[row][col] == '*':
+                    pygame.draw.rect(self.window, YELLOW, rect)
+
+                pygame.draw.rect(self.window, BLACK, rect, width=1)
+
+    def display(self, main_window):
+        main_window.blit(pygame.transform.scale(self.window, main_window.get_size()), (0,0))
+
+
+    def start(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def load_file(self, file= pathlib.Path(__file__).parent.absolute() / 'board.ini'):
+        parser = configparser.ConfigParser()
         parser.read(file)
         #self.tileset = parser.get("board","image")
-        self.board = parser.get("board","board")
+        print(parser['board'])
+        self.board = parser.get("board","start").split("\n")
+        # print(parser.sections())
         for section in parser.sections():
             if len(section) == 1:
                 desc = dict(parser.items(section))
+                # print(desc)
                 self.key[section] = desc
-        self.width = len(self.map[0])
-        self.height = len(self.map)
+        self.width = len(self.board[0])
+        self.height = len(self.board)
+        # print(self.key)
+        # print(self.width)
+        # print(self.height)
 
     def get_tile(self, x, y):
         try:
-            char = self.map[y][x]
+            char = self.board[y][x]
         except IndexError:
-            return
+            return {}
         try:
             return self.key[char]
         except KeyError:
             return {}
+
+class Tile(object):
+    def __init__(self):
+        pass
